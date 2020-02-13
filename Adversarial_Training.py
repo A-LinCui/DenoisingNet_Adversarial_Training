378bd8549eb63cde6b18ad583a8c64307d27abc1
zhaojb17 <53024459+zhaojb17@users.noreply.github.com>
2020-02-13 12:52:07 +0800

    Add files via upload

--- Adversarial_Training.py
+++ Adversarial_Training.py
@@ -6,22 +6,46 @@ from torch.nn import DataParallel
 import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
+from torch.optim.lr_scheduler import MultiStepLR
 
 import foolbox
 
 import numpy as np
 import os
+import csv
+import time
 import argparse
 import matplotlib.pyplot as plt
 
-from Denoising_ResNet import Denoising_ResNet18
+from Denoising_ResNet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
 
-parser = argparse.ArgumentParser(description='Adversarial Attack')
-parser.add_argument('-perturbation_threshold', dest='threshold', type=float, default=1e-8, help='maximum threshold')
+def load_model(basic_model, whether_denoising, filter_type, ksize):
+    if basic_model == 'ResNet18':
+        model = ResNet18(whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
+    elif basic_model == 'ResNet34':
+        model = ResNet34(whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
+    elif basic_model == 'ResNet50':
+        model = ResNet50(whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
+    elif basic_model == 'ResNet101':
+        model = ResNet101(whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
+    elif basic_model == 'ResNet152':
+        model = ResNet152(whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
+
+    model.cuda()
+    model = DataParallel(model)
+
+    return model
+
+parser = argparse.ArgumentParser(description='Adversarial Training for Denoising ResNet')
+parser.add_argument('-basic_ResNet', dest='basic_model', type=str, default='ResNet18', help='the basic ResNet model including ResNet18/ResNet34/ResNet50/ResNet101')
+parser.add_argument('-whether_denoising', dest='whether_denoising', type=bool, default=True, help='whether add denoising block into ResNet')
+parser.add_argument('-filter_type', dest='filter_type', type=str, default='Mean_Filter', help='Mean_Filter/Median_Filter/Gaussian_Filter')
+parser.add_argument('-kernel_size', dest='ksize', type=int, default=3, help='kernel size of the filter')
 parser.add_argument('-epoches', dest='epoches', type=int, default=200, help='epoches')
+parser.add_argument('-perturbation_threshold', dest='threshold', type=float, default=1e-8, help='maximum threshold')
 parser.add_argument('-batch_size', dest='batch_size', type=int, default=64, help='batch_size')
 parser.add_argument('-num_workers', dest='num_workers', type=int, default=32, help='num_workers')
-parser.add_argument('-learning_rate', dest='lr', type=float, default=1e-4, help='adv_learning_rate')
+parser.add_argument('-learning_rate', dest='lr', type=float, default=1e-3, help='adv_learning_rate')
 parser.add_argument('-weight_decay', dest='weight_decay', type=float, default=1e-4, help='weight_decay')
 parser.add_argument('-momentum', dest='momentum', type=float, default=0.9, help='momentum')
 
@@ -30,11 +54,7 @@ args = parser.parse_args()
 os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 2, 3"
 
 # Load the model
-model = Denoising_ResNet18()
-model.cuda()
-model = DataParallel(model)
-
-GPU_COUNT = torch.cuda.device_count()
+model = load_model(basic_model=args.basic_model, whether_denoising=args.whether_denoising, filter_type=args.filter_type, ksize=args.ksize)
 
 # Setup the transformation
 transform_train = transforms.Compose([
@@ -48,50 +68,89 @@ transform_test = transforms.Compose([
 ])
 
 # Load the dataset
+GPU_COUNT = torch.cuda.device_count()
 testset = torchvision.datasets.CIFAR10(root='/home/eva_share/datasets/cifar10', train=False, download=True, transform=transform_test)
 trainset = torchvision.datasets.CIFAR10(root='/home/eva_share/datasets/cifar10', train=True, download=True, transform=transform_train)
-testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size*GPU_COUNT, shuffle=False, num_workers=args.num_workers)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size*GPU_COUNT, shuffle=False, num_workers=args.num_workers)
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size * GPU_COUNT, shuffle=False, num_workers=args.num_workers)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size * GPU_COUNT, shuffle=False, num_workers=args.num_workers)
+
+# Establish the training log
+with open(args.basic_model + 'adv_training_log.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(["epoch", "test_clean_acc", "test_adv_acc", "train_clean_loss", "train_adv_loss", "train_loss", "train_clean_acc", "train_adv_acc"])
+
+# Training settings
+criterion = nn.CrossEntropyLoss()
+params = list(model.parameters())
+optimizer = optim.SGD(params, lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum, nesterov=True)
+scheduler = MultiStepLR(optimizer, milestones=[int(epoch/2), int(epoch*3/4), int(epoch*7/8)], gamma=0.1)
+
+# Record the best accuracy
+best_test_clean_acc, best_test_adv_acc, best_epoch = 0, 0, 0
 
 for epoch in range(args.epoches):
 
-    # Test
+    print("basic model: {}, whether denoising: {}, filter type: {}, kernel size: {}, max allowed perturbation: {}".format(args.basic_model, args.whether_denoising, args.filter_type, args.ksize, args.threshold))
+
+    scheduler.step()
+
+    # Test on the testset
     model.eval()
-    total, correct, robustness = 0, 0, 0
+    test_total, test_correct, test_robustness = 0, 0, 0
 
     # Setup the adversary
     fmodel = foolbox.models.PyTorchModel(model, bounds=(-255, 255), num_classes=10, device='Parrallel')
     attack = foolbox.attacks.FGSM(fmodel, distance=foolbox.distances.MeanSquaredDistance)
 
+    start_time = time.time()
+
     for (images, labels) in testloader:
         images = images.numpy()
         labels = labels.numpy()
-        total += images.shape[0]
-        correct += np.sum(fmodel.forward(images).argmax(axis=-1) == labels)
+        test_total += images.shape[0]
+        test_correct += np.sum(fmodel.forward(images).argmax(axis=-1) == labels)
         adversarials = attack(images, labels, unpack=False)
         for adversarial in adversarials:
             if adversarial.perturbed is None or adversarial.distance.value > args.threshold:
-                robustness += 1
+                test_robustness += 1
 
-    print("epoch:{} Classification accuracy on clean testset: {:.3f}".format(epoch, correct / total))
-    print("epoch:{} Classification accuracy on adversarial testset: {:.3f}".format(epoch, robustness / total))
+    test_acc, test_adv_acc = test_correct / test_total, test_robustness / test_total
 
-    # Train
-    total, correct, robustness = 0, 0, 0
+    # Record the time on the testset
+    end_time = time.time()
+    testset_total_time = end_time - start_time
+
+    # Save the present best model and its statistics
+    if test_adv_acc > best_test_adv_acc:
+        best_epoch = epoch
+        best_test_adv_acc = test_adv_acc
+        best_test_clean_acc = test_acc
+        torch.save(model.state_dict(), args.basic_model + '.pkl')
+
+    print("Present best adversarial model ----- best epoch: {} clean_test_acc: {:.3f} adv_test_acc: {:.3f}".format(best_epoch, best_test_clean_acc, best_test_adv_acc))
+    print("epoch:{} Classification accuracy on clean testset: {:.3f}".format(epoch, test_acc))
+    print("epoch:{} Classification accuracy on adversarial testset: {:.3f}".format(epoch, test_adv_acc))
+
+    # Test and Train on the trainset
+    train_total, train_correct, train_robustness = 0, 0, 0
+    train_clean_loss, train_adv_loss, train_loss = 0, 0, 0
+
+    start_time = time.time()
+
     for (images, labels) in trainloader:
         fmodel = foolbox.models.PyTorchModel(model, bounds=(-255, 255), num_classes=10, device='Parrallel')
         attack = foolbox.attacks.FGSM(fmodel, distance=foolbox.distances.MeanSquaredDistance)
         images = images.numpy()
         labels = labels.numpy()
-        total += images.shape[0]
-        correct += np.sum(fmodel.forward(images).argmax(axis=-1) == labels)
+        train_total += images.shape[0]
+        train_correct += np.sum(fmodel.forward(images).argmax(axis=-1) == labels)
 
         adversarials = attack(images, labels, unpack=False)
         adversarial_images, adversarial_orig_labels = [], []
 
         for adversarial in adversarials:
             if adversarial.perturbed is None or adversarial.distance.value > args.threshold:
-                robustness += 1
+                train_robustness += 1
             else:
                 adversarial_images.append(adversarial.perturbed)
                 adversarial_orig_labels.append(adversarial.original_class)
@@ -101,9 +160,6 @@ for epoch in range(args.epoches):
 
         # Adversarial Training Both on Adversarial Examples and Clean Inputs
         model.train()
-        criterion = nn.CrossEntropyLoss()
-        params = list(model.parameters())
-        optimizer = optim.SGD(params, lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum, nesterov=True)
         adv_outputs = model(adversarial_images)
         adv_loss = criterion(adv_outputs, adversarial_orig_labels.long())
         clean_outputs = model(torch.from_numpy(images).cuda())
@@ -112,8 +168,62 @@ for epoch in range(args.epoches):
         loss = clean_loss + adv_loss
         loss.backward()
         optimizer.step()
+        train_clean_loss += clean_loss.item()
+        train_adv_loss += adv_loss.item()
+        train_loss = train_adv_loss + train_clean_loss
         model.eval()
 
-    print("epoch:{} Classification accuracy on clean trainset: {:.3f}".format(epoch, correct / total))
-    print("epoch:{} Classification accuracy on adversarial trainset: {:.3f}".format(epoch,  robustness / total))
-    print()
+    # Record the time on the trainset
+    end_time = time.time()
+    trainset_total_time = end_time - start_time
+
+    train_acc, train_adv_acc = train_correct / train_total, train_robustness / train_total
+    print("epoch:{} train_clean_loss: {:.3f} train_adv_loss: {:.3f} train_total_loss: {:.3f}".format(epoch, train_clean_loss, train_adv_loss, train_loss))
+    print("epoch:{} Classification accuracy on clean trainset: {:.3f}".format(epoch, train_acc))
+    print("epoch:{} Classification accuracy on adversarial trainset: {:.3f}\n".format(epoch,  train_adv_acc))
+    print("epoch:{} Consumed time on the testset: {:.5f}s  on the trainset: {:.5f}s".format(epoch, testset_total_time, trainset_total_time))
+
+    # Recond the statistics
+    with open(args.basic_model + 'adv_training_log.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([epoch, test_acc, test_adv_acc, train_clean_loss, train_adv_loss, train_loss, train_acc, train_adv_acc])
+
+    # Load the statistics in the training log
+    epoch, test_acc, testset_adv_acc, train_clean_loss, train_adv_loss, train_loss, train_acc, train_adv_acc = [], [], [], [], [], [], [], []
+    with open(args.basic_model + 'adv_training_log.csv', 'a') as f:
+        reader = csv.reader(f)
+        for i in reader:
+            if i > 0:
+                epoch.append(i[0])
+                test_acc.append(i[1])
+                test_adv_acc.append(i[2])
+                train_clean_loss.append(i[3])
+                train_adv_loss.append(i[4])
+                train_loss.append(i[5])
+                train_acc.append(i[6])
+                train_adv_acc.append(i[7])
+
+    # Generate the training accuracy line
+    plt.figure()
+    plt.plot(epoch, test_acc, label='test_accuracy', linewidth=2, color='r')
+    plt.plot(epoch, test_adv_acc, label='test_adv_accuracy', linewidth=2, color='b')
+    plt.plot(epoch, train_acc, label='train_accuracy', linewidth=2, color='y')
+    plt.plot(epoch, train_adv_acc, label='train_adv_accuracy', linewidth=2, color='g')
+    plt.xlabel("epoch")
+    plt.ylabel("accuracy")
+    plt.xlim(-1, max(epoch) + 1)
+    plt.ylim(0, 1.0)
+    plt.title("training accuracy")
+    plt.savefig("train_accuracy.png")
+
+    # Generate the training loss line
+    plt.figure()
+    plt.plot(epoch, train_loss, label='train_total_loss', linewidth=2, color='r')
+    plt.plot(epoch, train_clean_loss, label='train_clean_loss', linewidth=2, color='b')
+    plt.plot(epoch, train_adv_loss, label='train_adv_loss', linewidth=2, color='y')
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.xlim(-1, max(epoch) + 1)
+    plt.ylim(0, max(train_loss))
+    plt.title("training loss on the trainset")
+    plt.savefig("train_loss.png")
--- Denoising_ResNet.py
+++ Denoising_ResNet.py
@@ -1,5 +1,5 @@
 """
-ResNet with Mean Filter Denoising Block in PyTorch.
+ResNet with Denoising Blocks in PyTorch on CIAFR10.
 
 Reference:
 [1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
@@ -7,11 +7,18 @@ Reference:
 [2] Cihang Xie, Yuxin Wu, Laurens van der Maaten, Alan Yuille, Kaiming He
     Feature Denoising for Improving Adversarial Robustness. arXiv:1812.03411
 
+Explanation:
+[1] If 'whether_denoising' is True, a ResNet with two denoising blocks will be created.
+    In contrast 'whether_denoising' is False, a normal ResNet will be created.
+[2] 'filter_type' decides which denoising operation the denoising block will apply.
+    Now it includes 'Median_Filter' 'Mean_Filter' and 'Gaussian_Filter'.
+[3] 'ksize' means the kernel size of the filter.
 """
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import kornia
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -36,7 +43,6 @@ class BasicBlock(nn.Module):
         out += self.shortcut(x)
         out = F.relu(out)
         return out
-
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -65,107 +71,32 @@ class Bottleneck(nn.Module):
         out = F.relu(out)
         return out
 
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
-
-class Mean_Filter(nn.Module):
-    def __init__(self, k_size):
-        super(Mean_Filter, self).__init__()
-        self.k_size = k_size
-        if k_size % 2 == 0:
-            print('Warning: k_size must be odd!')
-
-    def forward(self, x):
-        new_x = torch.zeros_like(x)
-        for i in range(x.shape[0]):
-            for j in range(x.shape[1]):
-                for m in range(x.shape[2]):
-                    for n in range(x.shape[3]):
-                        total = 0
-                        for k in range(max(m - int((self.k_size - 1) / 2), 0), min(m + 1 + int((self.k_size - 1) / 2), x.shape[2])):
-                            for g in range(max(n - int((self.k_size - 1) / 2), 0), min(n + 1 + int((self.k_size - 1) / 2), x.shape[3])):
-                                total += 1
-                                new_x[i][j][m][n] += x[i][j][k][g]
-                        new_x[i][j][m][n] /= total
-        return new_x
-
-
-class Median_Filter(nn.Module):
-    def __init__(self, k_size):
-        super(Median_Filter, self).__init__()
-        self.k_size = k_size
-        if k_size % 2 == 0:
-            print('Warning: k_size must be odd!')
-
-    def forward(self, x):
-        new_x = torch.zeros_like(x)
-        for i in range(x.shape[0]):
-            for j in range(x.shape[1]):
-                for m in range(x.shape[2]):
-                    for n in range(x.shape[3]):
-                        total = 0
-                        candidates = torch.zeros((min(m + 1 + int((self.k_size - 1) / 2), x.shape[2]) - max(m - int((self.k_size - 1) / 2), 0)) * (min(n + 1 + int((self.k_size - 1) / 2), x.shape[3]) - max(n - int((self.k_size - 1) / 2), 0)), 1)
-                        for k in range(max(m - int((self.k_size - 1) / 2), 0), min(m + 1 + int((self.k_size - 1) / 2), x.shape[2])):
-                            for g in range(max(n - int((self.k_size - 1) / 2), 0), min(n + 1 + int((self.k_size - 1) / 2), x.shape[3])):
-                                candidates[total] = x[i][j][k][g]
-                                total += 1
-                        candidates, _ = torch.sort(candidates)
-                        new_x[i][j][m][n] = candidates[int((candidates.shape[0] - 1) / 2)]
-        return new_x
-
-
-class denoising_box(nn.Module):
+class denoising_block(nn.Module):
     def __init__(self, in_planes, ksize, filter_type):
-        super(denoising_box, self).__init__()
+        super(denoising_block, self).__init__()
         self.in_planes = in_planes
-        if filter_type == 'Mean_Filter':
-            self.filter = Mean_Filter(ksize)
-        elif filter_type == 'Median_Filter':
-            self.filter = Median_Filter(ksize)
+        self.ksize = ksize
+        self.filter_type = filter_type
         self.conv = nn.Conv2d(in_channels=in_planes, out_channels=in_planes, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
         new_x = x
-        x_denoised = self.filter(x)
-        x_denoised = self.conv(x_denoised)
+        if self.filter_type == 'Median_Filter':
+            x_denoised = kornia.median_blur(x, (self.ksize, self.ksize))
+        elif self.filter_type == 'Mean_Filter':
+            x_denoised = kornia.box_blur(x, (self.ksize, self.ksize))
+        elif self.filter_type == 'Gaussian_Filter':
+            x_denoised = kornia.gaussian_blur2d(x, (self.ksize, self.ksize), (0.3 * ((x.shape[3] - 1) * 0.5 - 1) + 0.8, 0.3 * ((x.shape[2] - 1) * 0.5 - 1) + 0.8))
         new_x = x + x_denoised
         return new_x
 
-
-class Denoising_ResNet(nn.Module):
-    def __init__(self, filter_type, block, num_blocks, num_classes=10):
-        super(Denoising_ResNet, self).__init__()
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10, whether_denoising=False, filter_type="Mean_Filter", ksize=3):
+        super(ResNet, self).__init__()
+        if whether_denoising:
+            self.denoising_block1 = denoising_block(in_planes=64, ksize=ksize, filter_type=filter_type)
+            self.denoising_block2 = denoising_block(in_planes=64, ksize=ksize, filter_type=filter_type)
+        self.whether_denoising = whether_denoising
         self.in_planes = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
@@ -174,8 +105,6 @@ class Denoising_ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
-        self.denoising_box1 = denoising_box(in_planes=64, ksize=3, filter_type=filter_type)
-        self.denoising_box2 = denoising_box(in_planes=64, ksize=3, filter_type=filter_type)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -187,9 +116,11 @@ class Denoising_ResNet(nn.Module):
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-        out = self.denoising_box1(out)
+        if self.whether_denoising:
+            out = self.denoising_block1(out)
         out = self.layer1(out)
-        out = self.denoising_box2(out)
+        if self.whether_denoising:
+            out = self.denoising_block2(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
@@ -199,61 +130,36 @@ class Denoising_ResNet(nn.Module):
         return out
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+def ResNet18(whether_denoising=False, filter_type="Mean_Filter", ksize=3):
+    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes=10, whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
 
-def Denoising_ResNet18():
-    return Denoising_ResNet("Mean_Filter", BasicBlock, [2, 2, 2, 2])
+def ResNet34(whether_denoising=False, filter_type="Mean_Filter", ksize=3):
+    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes=10, whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
 
-def ResNet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
+def ResNet50(whether_denoising=False, filter_type="Mean_Filter", ksize=3):
+    return ResNet(Bottleneck, [3, 4, 6, 3], num_classes=10, whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
 
-def Denoising_ResNet34():
-    return Denoising_ResNet("Mean_Filter", BasicBlock, [3, 4, 6, 3])
+def ResNet101(whether_denoising=False, filter_type="Mean_Filter", ksize=3):
+    return ResNet(Bottleneck, [3, 4, 23, 3], num_classes=10, whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
 
-def ResNet50():
-    return ResNet(Bottleneck, [3, 4, 6, 3])
-
-def Denoising_ResNet50():
-    return Denoising_ResNet("Mean_Filter", Bottleneck, [3, 4, 6, 3])
-
-def ResNet101():
-    return ResNet(Bottleneck, [3, 4, 23, 3])
-
-def Denoising_ResNet101():
-    return Denoising_ResNet("Mean_Filter", Bottleneck, [3, 4, 23, 3])
-
-def ResNet152():
-    return ResNet(Bottleneck, [3, 8, 36, 3])
-
-def Denoising_ResNet152():
-    return Denoising_ResNet("Mean_Filter", Bottleneck, [3, 8, 36, 3])
+def ResNet152(whether_denoising=False, filter_type="Mean_Filter", ksize=3):
+    return ResNet(Bottleneck, [3, 8, 36, 3], num_classes=10, whether_denoising=whether_denoising, filter_type=filter_type, ksize=ksize)
 
 def test():
     from torch.autograd import Variable
-    net = Denoising_ResNet18()
+    net = ResNet18(whether_denoising=True, filter_type="Median_Filter", ksize=3)
     x = Variable(torch.randn(1, 3, 32, 32), requires_grad=True)
     y = net(x)
-    y.backward(x)
-
-def filter_test():
-    from torch.autograd import Variable
-    filter = Median_Filter(3)
-    x = Variable(torch.randn(1, 3, 32, 32), requires_grad=True)
-    y = filter(x)
     print(y)
-    y.backward(x)
-    print(x.grad)
 
-def denoising_box_test():
+def denoising_block_test(filter_type):
     from torch.autograd import Variable
-    denoising_box1 = denoising_box(in_planes=32, ksize=3, filter_type='Mean_Filter')
-    x = Variable(torch.randn(1, 32, 32, 32), requires_grad=True)
-    y = denoising_box1(x)
+    denoising_block1 = denoising_block(in_planes=32, ksize=3, filter_type=filter_type)
+    x = Variable(torch.ones(2, 64, 32, 32), requires_grad=True)
+    y = denoising_block1(x)
     print(y)
     y.backward(x)
     print(x.grad)
 
 # test()
-# filter_test()
-denoising_box_test()
+# denoising_block_test('Median_Filter')
